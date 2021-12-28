b84d7e4843c34e1af37b975f201f73e8055eb828
marshalonah <marshalonah@gmail.com>
2021-12-28 11:01:44 +0100

    camera movement

--- assets/Scripts/CameraController.ts
+++ assets/Scripts/CameraController.ts
@@ -1,32 +1,86 @@
 
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Camera, Vec2, Event, EventTouch } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('CameraController')
-export class CameraController extends Component {
-    // [1]
-    // dummy = '';
+export class CameraController extends Component{
+    
+    @property(Node)
+    public Camera : Node;
 
-    // [2]
-    // @property
-    // serializableDummy = 0;
+    private TouchMoved : boolean;
+    private Pos_Offset : Vec2
+
+    onLoad(){
+        this.node.on("touch-start", this.onTouchBegan, this);
+        this.node.on("touch-move", this.onTouchMove, this);
+        this.node.on("touch-cancel", this.onTouchCancel, this);
+        this.node.on("touch-end", this.onTouchEnd, this);
+
+        this.TouchMoved = false;
+        this.Pos_Offset = new Vec2();
+    }
 
     start () {
-        // [3]
+        
     }
+
+    onTouchBegan(event : Event){
+        this.TouchMoved = false;
+        event.propagationStopped = true;
+        // this.stopPropagationIfTargetIsMe(event)
+    }
+
+    onTouchMove(event : EventTouch){
+        console.log("Touch Move");
+
+        let touches = event.getAllTouches();
+            
+        let touch1 = touches[0];
+        let delta1 = touch1.getDelta();
+
+        this.Pos_Offset.x = delta1.x;
+        this.Pos_Offset.y = delta1.y;
+
+        let cam_pos = this.Camera.getPosition();
+
+        cam_pos.x = cam_pos.x - this.Pos_Offset.x;
+        cam_pos.y = cam_pos.y - this.Pos_Offset.y;
+
+        this.Camera.setPosition(cam_pos);
+
+        // this.Cancel_Inner_Touch(event);
+        this.stopPropagationIfTargetIsMe(event);
+
+    }
+
+    onTouchCancel(event : Event){
+        this.stopPropagationIfTargetIsMe(event);
+    }
+
+    onTouchEnd(event : Event){
+        if(this.TouchMoved){
+            event.propagationStopped = true;
+        }else{
+            this.stopPropagationIfTargetIsMe(event);
+        }
+    }
+
+    stopPropagationIfTargetIsMe(event : Event){
+        if(event.eventPhase === Event.AT_TARGET && event.target == this.node){
+            event.propagationStopped = true;
+        }
+    }
+
+    // CancelInnerTouch(event : EventTouch){
+    //     var touch = event.touch;
+    //     let p1 = touch.getLocation();
+    //     var deltaMove = p1.subtract(touch.getStartLocation());
+    //     if(deltaMove.m)
+    // }
 
     // update (deltaTime: number) {
     //     // [4]
     // }
 }
 
-/**
- * [1] Class member could be defined like this.
- * [2] Use `property` decorator if your want the member to be serializable.
- * [3] Your initialization goes here.
- * [4] Your update function goes here.
- *
- * Learn more about scripting: https://docs.cocos.com/creator/3.0/manual/en/scripting/
- * Learn more about CCClass: https://docs.cocos.com/creator/3.0/manual/en/scripting/ccclass.html
- * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.0/manual/en/scripting/life-cycle-callbacks.html
- */
